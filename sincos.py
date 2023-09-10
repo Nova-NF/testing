e9ecfef1ec2765a12eae54b4325276b6c902c167
Nova-NF <144618201+Nova-NF@users.noreply.github.com>
2023-09-11 00:47:11 +0700

    Add files via upload

---
+++ sincos.py
@@ -0,0 +1,10 @@
+import math
+
+radian = math.radians(45.0)
+hasil_sinus = math.sin(radian)
+hasil_cosinus = math.cos(radian)
+hasil_tangen = math.tan(radian)
+
+print("hasil sinus 45 derajat = ", hasil_sinus)
+print("hasil cosinus 45 derajat = ", hasil_cosinus)
+print("hasil tangen 45 derajat = ", hasil_tangen)
